155fee5dafc597b60012c361ca877a39ef83af17
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-14 13:53:17 -0400

    Update tl_member.php

--- system/modules/handyscribe_registration/dca/tl_member.php
+++ system/modules/handyscribe_registration/dca/tl_member.php
@@ -12,7 +12,7 @@
 
  /* Extend the tl_user palettes */
 foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
-    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('email;', 'email;{handyscribe_legend},ein;', $v);
+    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{handyscribe_legend},ein;', $v);
 }
 
 /* Add fields to tl_user */
