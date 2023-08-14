a7c466e4b3c6e4a70e00a5d761de565ac78b0a33
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-14 14:11:43 -0400

    Update tl_member.php

--- system/modules/handyscribe_registration/dca/tl_member.php
+++ system/modules/handyscribe_registration/dca/tl_member.php
@@ -10,8 +10,6 @@
  * @license    http://opensource.org/licenses/lgpl-3.0.html
 **/
 
-use Contao\DataContainer;
-
  /* Extend the tl_user palettes */
 foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
     $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{handyscribe_legend},ein;', $v);
@@ -22,7 +20,6 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['ein'] = array
 (
     'search'                  => true,
     'sorting'                 => true,
-    'flag'                    => DataContainer::SORT_INITIAL_LETTER_BOTH,
     'inputType'               => 'text',
     'eval'                    => array('mandatory'=>true, 'maxlength'=>255, 'feEditable'=>true, 'feViewable'=>true, 'feGroup'=>'personal', 'tl_class'=>'w50'),
     'sql'                     => "varchar(255) NOT NULL default ''"
