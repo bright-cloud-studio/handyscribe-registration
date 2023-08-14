4b0677efa20a8805eacf3fb8dff07d8b02ede819
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-14 13:43:11 -0400

    Create tl_user.php

---
+++ system/modules/handyscribe_registration/languages/en/tl_user.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Handyscribe Registration
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/handyscribe-registration
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_user']['ein']          = array('Employer Identification Number (EIN)', 'The EIN (Employer Identification Number) for this user.');
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_user']['handyscribe_legend'] = 'Handyscribe\'s Custom Fields';
